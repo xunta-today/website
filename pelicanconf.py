f72c9d50ead52fddfaad5d1323e079078d86ccc2
jin10086 <igaojin@qq.com>
2019-11-30 22:57:46 +0800

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,15 +20,15 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (
-    ("Pelican", "http://getpelican.com/"),
-    ("Python.org", "http://python.org/"),
-    ("Jinja2", "http://jinja.pocoo.org/"),
-    ("You can modify those links in your config file", "#"),
-)
+# LINKS = (
+#     ("Pelican", "http://getpelican.com/"),
+#     ("Python.org", "http://python.org/"),
+#     ("Jinja2", "http://jinja.pocoo.org/"),
+#     ("You can modify those links in your config file", "#"),
+# )
 
 # Social widget
-SOCIAL = (("You can add links in your config file", "#"), ("Another social link", "#"))
+# SOCIAL = (("You can add links in your config file", "#"), ("Another social link", "#"))
 
 DEFAULT_PAGINATION = 10
 
